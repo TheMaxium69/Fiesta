7c79fddc4e099cfaa2bf88c942a37f4f65d890cf
TheMaxium69 <mxm.tournier@gmail.com>
2020-10-30 14:36:55 +0100

    all

---
+++ index.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    frame: false,  
+    title: "Festia",
+    width: 800,
+    height: 600,
+    webPreferences: {
+      
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
